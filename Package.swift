2b3a7a4e60ce49e2e1a16b020623d81195d9fec9
znhyo <102845801+znhyo@users.noreply.github.com>
2025-01-08 17:41:02 +0800

    add Link CoreMotion

--- Package.swift
+++ Package.swift
@@ -47,6 +47,7 @@ let package = Package(
                 .linkedFramework("CoreTelephony"),
                 .linkedFramework("AVFoundation"),
                 .linkedFramework("OpenGLES"),
+                .linkedFramework("CoreMotion"),
                 .linkedFramework("Accelerate"),
                 .linkedFramework("MetalKit"),
                 .linkedFramework("MobileCoreServices"),
